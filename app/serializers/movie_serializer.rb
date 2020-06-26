4b256395f48581a67f6ff052a971e6687ec34b70
Angela Nguyen <angethuy@gmail.com>
2020-06-25 21:38:54 -0700

    added inventory_available to serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,7 @@
 class MovieSerializer < ActiveModel::Serializer
   attribute :id, if: -> { object.id != nil }
+  attribute :available_inventory, if: -> { object.available_inventory && object.available_inventory != nil }
 
   attributes :title, :overview, :release_date, :image_url, :external_id
+  
 end
